fb87dcd9236eeae494d157038010e00f79f97b15
Marius <marius@freshcolors.ro>
2015-04-11 16:24:20 +0300

    Add possibility to specify ssh port and identity file to use; Alter the formatting a little bit

--- durant/main.py
+++ durant/main.py
@@ -7,11 +7,14 @@ import durant
 from durant.cli import output
 from durant.colors import colors
 
+
 def usage():
     print(durant.__doc__)
 
+
 def version():
     print('Version %s' % durant.__version__)
+
 
 def main():
     try:
@@ -26,12 +29,12 @@ def main():
             sys.exit(1)
         elif '-v' in args or '--version' in args:
             version()
-            sys.exit(1)             
+            sys.exit(1)
         elif 'deploy' in args:
             dry_run = True if '-n' in args or '--dry-run' in args else False
-            
-            try:                
-                stage = args[args.index('deploy')+1]
+
+            try:
+                stage = args[args.index('deploy') + 1]
             except IndexError:
                 usage()
                 sys.exit(1)
@@ -39,27 +42,30 @@ def main():
                 start = time.time()
 
                 d = durant.Deployer()
-                
+
                 d.print('Starting deployment\n', color=colors.YELLOW)
 
                 try:
                     d.deploy(stage, dry_run)
-                    
+
                     end = time.time()
-                   
+
                     d.print_nl()
-                    d.print('Deployment complete (%.3f seconds)' % (end - start), color=colors.YELLOW) 
+                    d.print('Deployment complete (%.3f seconds)' %
+                            (end - start),
+                            color=colors.YELLOW)
                 except Exception as e:
                     end = time.time()
 
                     d.print_error(str(e))
                     d.print_nl()
-                    d.print('Deployment failed (%.3f seconds)' % (end - start), color=colors.YELLOW) 
+                    d.print('Deployment failed (%.3f seconds)' % (end - start),
+                            color=colors.YELLOW)
                     sys.exit(1)
         else:
             usage()
             sys.exit(1)
 
+
 if __name__ == "__main__":
     main()
-
